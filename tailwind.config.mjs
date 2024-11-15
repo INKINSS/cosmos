4cd14440a13cfe6d5ffd66004a451980ecfd094b
Martin Samuel Perez Montenegro <martinsamuelpp@gmail.com>
2024-11-15 08:36:54 -0600

    configuración de tema para shadcn

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+    darkMode: ['class'],
+    content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
-	},
-	plugins: [],
+    	extend: {
+    		colors: {
+    			customGreen: 'rgb(38,133,59)',
+    			customGray: 'rgb(138, 138, 138)'
+    		},
+    		borderRadius: {
+    			lg: 'var(--radius)',
+    			md: 'calc(var(--radius) - 2px)',
+    			sm: 'calc(var(--radius) - 4px)'
+    		}
+    	}
+    },
+	plugins: [require("tailwindcss-animate")],
 }
