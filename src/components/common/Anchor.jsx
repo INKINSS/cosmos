87988b3f10d5d37400b89008860ccc76c5d20890
Martin Samuel Perez Montenegro <martinsamuelpp@gmail.com>
2024-11-16 12:05:01 -0600

    Actualizar estilos de Anchor, agregar componente ButtonDownload y modificar Logo para incluir texto adicional

--- src/components/common/Anchor.jsx
+++ src/components/common/Anchor.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Anchor = ({ label, href }) => {
   return (
-    <li className="flex justify-end">
+    <li className="flex justify-end py-2 pr-5 hover:bg-customGreen hover:text-white transform duration-100 md:hover:bg-white md:hover:text-black md:duration-0">
       <a className="text-[1.3rem]" href={href}>
         {label}
       </a>
